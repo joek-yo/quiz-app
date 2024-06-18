ba3b103bd8b979d41f9a32a4e730371d1188db47
joek-yo <jkihiu129@gmail.com>
2024-06-18 14:03:00 +0300

    updated the html files and app.js file

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-// app.js
-
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const questions = require('./questions.json');
 
-// Serve static files from the public directory
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define routes
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 });
 
-// Start the server
+app.get('/quiz', (req, res) => {
+  res.render('quiz', { questions });
+});
+
+app.post('/submit', (req, res) => {
+  const score = calculateScore(req.body);
+  res.render('result', { score });
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+function calculateScore(answers) {
+  let score = 0;
+  for (let i = 0; i < questions.length; i++) {
+    const correctAnswer = questions[i].answer;
+    const userAnswer = answers[`answer${i}`];
+    if (correctAnswer === userAnswer) {
+      score++;
+    }
+  }
+  return score;
+}
